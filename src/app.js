3958e7a4c2bf8a2a273237f922eafa3372d138af
leovaldez08 <116164199+leovaldez08@users.noreply.github.com>
2023-01-23 17:48:07 +0530

    1 file modified

--- src/app.js
+++ src/app.js
@@ -1,18 +1,31 @@
 class Blog {
   // Progression 1: Add 2 methods - addTitle(), and addDescription()
-  constructor(title, detail){
-    this.title = title
-    this.detail = detail
+  constructor(title, detail) {
+    this.title = title;
+    this.detail = detail;
   }
   addTitle() {
     var title_card = document.createElement('h1');
-    title_card.setAttribute("id","blog-title");
+    title_card.setAttribute('id', 'blog-title');
     console.log(title_card);
     document.getElementById('card-text').appendChild(title_card);
     title_card.innerHTML += this.title;
+  }
+
+  addDescription() {
+    var description = document.createElement('p');
+    description.setAttribute('id', 'blog-description');
+    console.log(description);
+    document.getElementById('card-text').appendChild(description);
+    description.innerHTML += this.detail;
   }
 }
 
 // Progression 2: Setup an event listner - add two functions
 // 1. `helperAddPost()`
 // 2. `helperPost()`
+
+// 1.
+function helperAddPost() {
+  document.getElementById('popupContact').style.display = 'inline';
+}
